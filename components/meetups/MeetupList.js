0429c737e844cbe9399caccb81ea469f30159814
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-05-20 20:47:23 +0300

    final

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -4,16 +4,20 @@ import classes from "./MeetupList.module.css";
 function MeetupList(props) {
   return (
     <ul className={classes.list}>
-      {props.meetups.map(meetup => (
-        <MeetupItem
-          key={meetup._id}
-          id={meetup._id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
-          isFav={meetup.isFav}
-        />
-      ))}
+      {props.meetups.length ? (
+        props.meetups.map(meetup => (
+          <MeetupItem
+            key={meetup._id}
+            id={meetup._id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+            isFav={meetup.isFav}
+          />
+        ))
+      ) : (
+        <p>No meetups added yet.</p>
+      )}
     </ul>
   );
 }
--- pages/favs/index.js
+++ pages/favs/index.js
@@ -1,7 +1,31 @@
-//our-domain/new-meetup
+//our-domain/favs
 
-const FavsPage = () => {
-  return <div></div>;
+import { MongoClient } from "mongodb";
+import MeetupList from "../../components/meetups/MeetupList";
+
+const FavsPage = props => {
+  return <MeetupList meetups={props.favMeetups} />;
 };
+
+export async function getStaticProps(context) {
+  const client = await MongoClient.connect(
+    "mongodb+srv://ya:qwe123zx@cluster0.kxotm.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  //return just _id
+  const favMeetups = await await meetupsCollection
+    .find({ isFav: true })
+    .toArray();
+
+  //dont forget to close connection
+  client.close();
+
+  return {
+    props: {
+      favMeetups: favMeetups.map(fav => ({ ...fav, _id: fav._id.toString() })),
+    },
+  };
+}
 
 export default FavsPage;
