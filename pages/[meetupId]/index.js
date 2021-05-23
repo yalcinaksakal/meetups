ddf50489b9fd01780041c04e37ace6f3ac7d7e0d
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-05-23 04:28:07 +0300

    final

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -4,23 +4,34 @@ import MeetupDetail from "../../components/meetups/MeetupDetail";
 import Head from "next/head";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
-
+import ErrorPage from "next/error";
+import Spinner from "../../components/ui/Spinner";
 const MeetupDetails = () => {
   const id = useRouter().query.meetupId;
+
   const { isLoaded } = useSelector(state => state.meetups);
   const meetup = useSelector(state => state.meetups).meetups.find(
     meetup => meetup._id === id
   );
+
   return (
     <>
       <Head>
-        <title>{isLoaded ? meetup.title : "Meetup"} meetup</title>
+        <title>{isLoaded ? meetup?.title : "Meetup"} meetup</title>
         <meta
           name="description"
-          content={isLoaded ? meetup.description : "A meetup"}
+          content={isLoaded ? meetup?.description : "A meetup"}
         />
       </Head>
-      {isLoaded && <MeetupDetail {...meetup} />}
+      {isLoaded ? (
+        !meetup ? (
+          <ErrorPage statusCode="404" />
+        ) : (
+          <MeetupDetail {...meetup} />
+        )
+      ) : (
+        <Spinner />
+      )}
     </>
   );
 };
